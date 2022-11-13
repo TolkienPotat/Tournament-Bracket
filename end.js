8a4af037614e18b4bb7929299eac4a003e3785f5
TolkienPotat <66974977+TolkienPotat@users.noreply.github.com>
2022-11-12 20:29:01 -0800

    Update end.js

--- end.js
+++ end.js
@@ -6,3 +6,14 @@ function checkWinner(){
     document.getElementById("winText").innerHTML = winner + " wins!";
   }
 }
+
+function endAndReturn(){
+  
+  
+  blank = [];
+  blankJSON = JSON.stringify(blank);
+  localStorage.setItem("tmbeJSON", blankJSON);
+  localStorage.setItem("tmbdJSON", blankJSON);
+  localStorage.setItem("tmbJSON", blankJSON);
+  location.href='index.html';
+}
